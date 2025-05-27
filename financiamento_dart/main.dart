868add13d1f790f8ecb2dc18e21d43d22f95ae38
Coronel <160642659+helio000@users.noreply.github.com>
2025-05-27 10:03:54 -0300

    Add files via upload

--- financiamento_dart/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:financiamento_app/ui/financiamento_screen.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MaterialApp(title:'Financing App', home: FinanciamentoScreen()));
-}
